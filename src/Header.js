ebc45fa858b88a170fb94715624d5f78da3841c6
Katalin Kovács <katalin.kovacs@innosw.hu>
2022-02-28 17:32:11 +0100

    add header component

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="container">
+        <h1>Number Converter</h1>
+        <p>Description...</p>
+      </div>
+      <img src="#" alt="loudspeaker" />
+    </header>
+  );
+};
+
+export default Header;
