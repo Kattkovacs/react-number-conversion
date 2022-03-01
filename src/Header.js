25a79cc96a45d014b4a5dc730aa1c23cf1d8d123
Katalin Kovács <katalin.kovacs@innosw.hu>
2022-03-01 09:15:05 +0100

    add img to header

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,16 @@
+import loudspeaker from "./loudspeaker.svg";
 import React from "react";
 
 const Header = () => {
   return (
     <header className="header">
       <div className="container">
-        <h1>Number Converter</h1>
-        <p>Description...</p>
+        <div>
+          <h1>Number Converter</h1>
+          <p>Convert any number to text. Have fun!</p>
+        </div>
+        <img src={loudspeaker} alt="loudspeaker" />
       </div>
-      <img src="#" alt="loudspeaker" />
     </header>
   );
 };
